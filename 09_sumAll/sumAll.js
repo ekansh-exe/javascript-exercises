4599b3ef111c0ebad3c3f86a696e6cfa96e23fff
ekansh-exe <173880402+ekansh-exe@users.noreply.github.com>
2025-06-06 21:31:51 +0530

    completed these exercies again

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(val1,val2) {
+    let sum =0;
+    for(let i = val1; i<=val2;i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
