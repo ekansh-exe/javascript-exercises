5b822cf0a44efc6f23af900d3074ba8a9c8fea8f
ekansh-exe <173880402+ekansh-exe@users.noreply.github.com>
2025-06-07 12:41:41 +0530

    completed the exercise

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,10 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+    const a = (tempF - 32)*(5/9);
+    return Math.round(a * 10) / 10   
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  return Math.round(((tempC * 9) / 5 + 32) * 10) / 10;
 };
 
 // Do not edit below this line
